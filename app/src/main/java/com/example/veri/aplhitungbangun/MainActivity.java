557de6738f43ba99275b368a43f2dc6119979441
verianggoro <veryanggoro007@gmail.com>
2018-04-29 22:32:50 +0700

    Done Semua

--- app/src/main/java/com/example/veri/aplhitungbangun/MainActivity.java
+++ app/src/main/java/com/example/veri/aplhitungbangun/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.veri.aplhitungbangun;
 
-import android.support.annotation.IntegerRes;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,25 +9,24 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tvHasil;
-    private EditText editText, editText1;
-    private Button buttonKel, buttonluas, buttonDel;
+    TextView tvHasil;
+    EditText editText, editText1;
+    Button buttonKel, buttonluas, buttonDel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView TvHasil = (TextView) findViewById(R.id.tvhasil);
-        final EditText editText = (EditText) findViewById(R.id.ET_1);
-        final EditText editText1 = (EditText) findViewById(R.id.ET_2);
-        Button buttonKel = (Button) findViewById(R.id.btnKel);
-        Button buttonluas = (Button) findViewById(R.id.btnLuas);
-        Button buttonDel = (Button) findViewById(R.id.btnHapus);
+        tvHasil = (TextView) findViewById(R.id.tvhasil);
+        editText = (EditText) findViewById(R.id.ET_1);
+        editText1 = (EditText) findViewById(R.id.ET_2);
+        buttonKel = (Button) findViewById(R.id.btnKel);
+        buttonluas = (Button) findViewById(R.id.btnLuas);
+        buttonDel = (Button) findViewById(R.id.btnHapus);
+
+
         buttonluas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,18 +37,11 @@ public class MainActivity extends AppCompatActivity {
                 } else if (editText1.length() == 0) {
                     Toast.makeText(getApplication(), "Lebar Tidak boleh kosong", Toast.LENGTH_LONG).show();
                 } else {
-                    try {
-                        int panjang = Integer.parseInt(editText.getText().toString());
-                        int lebar = Integer.parseInt(editText1.getText().toString());
-                        int luas = panjang * lebar;
-                        TvHasil.setText(Integer.toString(luas));
-                        return;
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                    int panjang = Integer.parseInt(editText.getText().toString());
+                    int lebar = Integer.parseInt(editText1.getText().toString());
+                    int luas = panjang * lebar;
+                    tvHasil.setText(Integer.toString(luas));
                 }
-
-
             }
         });
         buttonKel.setOnClickListener(new View.OnClickListener() {
@@ -62,63 +54,45 @@ public class MainActivity extends AppCompatActivity {
                 } else if (editText1.length() == 0) {
                     Toast.makeText(getApplication(), "Lebar Tidak boleh kosong", Toast.LENGTH_LONG).show();
                 } else {
+                    int panjang = Integer.parseInt(editText.getText().toString());
+                    int lebar = Integer.parseInt(editText1.getText().toString());
+                    int luas =2*(panjang + lebar);
+                    tvHasil.setText(Integer.toString(luas));
+                }
+            }
+
+        });
+
+        buttonDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.getText().clear();
+                editText1.getText().clear();
+                tvHasil.setText("0");
+            }
+        });
+    }
+}
+
+        /*buttonKel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editText.length() == 0 && editText1.length() == 0) {
+                    Toast.makeText(getApplication(), "Panjang dan Lebar Harus Terisi", Toast.LENGTH_LONG).show();
+                } else if (editText.length() == 0) {
+                    Toast.makeText(getApplication(), "Panjang tidak boleh kosong", Toast.LENGTH_LONG).show();
+                } else if (editText1.length() == 0) {
+                    Toast.makeText(getApplication(), "Lebar Tidak boleh kosong", Toast.LENGTH_LONG).show();
+                } else {
                     try {
                         int panjang = Integer.parseInt(editText.toString());
                         int lebar = Integer.parseInt(editText1.toString());
-                        int Kel = (2*panjang) +(2*lebar);
-                        TvHasil.setText(Integer.toString(Kel));
+                        int Keliling = lebar*panjang;
+                        TvHasil.setText(Integer.parseInt(String.valueOf(Keliling)));
                         return;
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
             }
-        });
-        buttonDel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText("");
-                editText1.setText("");
-                TvHasil.setText("0");
-            }
-        });
-    }
-    /*
-        if (editText.length() == 0 && editText1.length() == 0) {
-            Toast.makeText(getApplication(), "Panjang dan Lebar Harus Terisi", Toast.LENGTH_LONG).show();
-        } else if (editText.length() == 0) {
-            Toast.makeText(getApplication(), "Panjang tidak boleh kosong", Toast.LENGTH_LONG).show();
-        } else if (editText1.length() == 0) {
-            Toast.makeText(getApplication(), "Lebar Tidak boleh kosong", Toast.LENGTH_LONG).show();
-        } else {
-            try {
-                int panjang = Integer.parseInt(editText.getText().toString());
-                int lebar = Integer.parseInt(editText1.getText().toString());
-                int luas = panjang * lebar;
-                txtView.setText(String.valueOf(luas));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-*/
-
-
-  /*
-        if (editText.length() == 0 && editText1.length() == 0) {
-            Toast.makeText(getApplication(), "Panjang dan Lebar Harus Terisi", Toast.LENGTH_LONG).show();
-        } else if (editText.length() == 0) {
-            Toast.makeText(getApplication(), "Panjang tidak boleh kosong", Toast.LENGTH_LONG).show();
-        } else if (editText1.length() == 0) {
-            Toast.makeText(getApplication(), "Lebar Tidak boleh kosong", Toast.LENGTH_LONG).show();
-        } else {
-            try {
-                int panjang = Integer.parseInt(editText.toString());
-                int lebar = Integer.parseInt(editText1.toString());
-                int kel = 2 * (panjang + lebar);
-                txtView.setText(String.valueOf(kel));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    */
-}
+        });*/
